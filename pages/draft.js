62241648d987d0b67b92519fa976194a33ce8c12
danilo leal <99835406+danilo-sl@users.noreply.github.com>
2022-03-26 10:59:14 -0300

    setup

---
+++ pages/draft.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Alpine from "alpinejs";
+
+export default function Draft() {
+  return (
+    <div className='m-4 p-16 bg-gray-100 rounded-sm'>
+      <p className='font-semibold text-lg'>
+        How to conduct user research in an agile environment
+      </p>
+      <p className=''>
+        A Designer’s Guide to Documenting Accessibility & User Interactions
+      </p>
+    </div>
+  );
+}
